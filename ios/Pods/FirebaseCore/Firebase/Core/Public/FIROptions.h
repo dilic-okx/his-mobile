4195c3578a419b32fb37a56f1c914aa87c81feee
dilic-okx <netframe@gmail.com>
2024-12-30 18:11:01 +0100

    Old keys removed

--- ios/Pods/FirebaseCore/Firebase/Core/Public/FIROptions.h
+++ ios/Pods/FirebaseCore/Firebase/Core/Public/FIROptions.h
@@ -31,8 +31,8 @@ NS_SWIFT_NAME(FirebaseOptions)
 + (nullable FIROptions *)defaultOptions NS_SWIFT_NAME(defaultOptions());
 
 /**
- * An iOS API key used for authenticating requests from your app, e.g.
- * @"AIzaSyDdVgKwhZl0sTTTLZ7iTmt1r3N2cJLnaDk", used to identify your app to Google servers.
+ * An iOS API key used for authenticating requests from your app
+ * used to identify your app to Google servers.
  */
 @property(nonatomic, copy, nullable) NSString *APIKey NS_SWIFT_NAME(apiKey);
 
